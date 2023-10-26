200da105fccf06a21ce292d72498a0ae16908496
Quaviur Rahman <user@Users-MacBook-Pro.local>
2023-10-27 01:38:59 +0600

    added login and registration functionality

--- models/User.js
+++ models/User.js
@@ -5,7 +5,8 @@ const SALT_WORK_FACTOR = 10;
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true},
+  email: { type: String, required: true, unique: true },
+  company: { type: String, required: true},
 });
 
 userSchema.pre('save', function (next) {
