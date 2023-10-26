44742eec40d5424c77d385266067fd78e1ba177e
Quaviur Rahman <user@Users-MacBook-Pro.local>
2023-10-27 00:57:18 +0600

    added login and registration functionality

--- app.js
+++ app.js
@@ -33,6 +33,23 @@ const options = {
 const specs = swaggerJsdoc(options);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
+// Middleware to check the JWT for the routes that require authentication
+const checkAuth = (req, res, next) => {
+  const token = req.header('Authorization');
+
+  if (!token) {
+    return res.status(401).json({ error: 'Access denied, please log in.' });
+  }
+
+  try {
+    const decoded = jwt.verify(token, config.jwtSecret);
+    req.user = decoded;
+    next();
+  } catch (ex) {
+    res.status(400).json({ error: 'Invalid token.' });
+  }
+};
+
 // Enable CORS for all routes
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://snap-z-fe.vercel.app'); 
@@ -45,7 +62,7 @@ app.use((req, res, next) => {
 app.use('/topics', topicsRouter);
 app.use('/posts', postsRouter);
 app.use("/dashboard", dashboardRouter);
-app.use("/auth",authenticationRouter);
+app.use("/auth", authenticationRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
