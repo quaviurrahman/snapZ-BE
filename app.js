b6ee37d83af027f035eab0f1bdf96ae7efd2571f
Quaviur Rahman <user@Users-MacBook-Pro.local>
2023-10-27 09:55:22 +0600

    added login and registration functionality

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const authenticationRouter = require('./routes/users.js')
 const cors = require ("cors");
 
 const app = express();
-app.use (cors());
+app.use (cors({origin : "*"}));
 const port = process.env.PORT || 3000;
 
 
